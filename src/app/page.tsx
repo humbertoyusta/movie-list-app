76a78e63a47e5f2cb2ec3ac92c3544f4decfe68e
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-22 01:18:57 +0100

    feat/add-fetch-lists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 import Header from "@/components/Header";
+import List from "@/components/List";
+import { sdk } from "@/graphql/client";
+import { MY_EMAIL_KEY } from "@/constants";
 
-export default function Home() {
-    return <Header title={"Movie List App"} />;
+export default async function Home() {
+    const { getMovieLists: movieLists } = await sdk.getMovieLists({
+        email: MY_EMAIL_KEY,
+    });
+
+    return (
+        <>
+            <Header title={"Movie List App"} />
+            <List movieLists={movieLists} />
+        </>
+    );
 }
