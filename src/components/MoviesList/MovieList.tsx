b8a907c58ee9db7be7f99986a60f4282822a1f71
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-24 19:30:45 +0100

    feat/handle-remove

--- src/components/MoviesList/MovieList.tsx
+++ src/components/MoviesList/MovieList.tsx
@@ -1,57 +1,32 @@
 "use client";
 
-import IMovie from "@/interfaces/IMovie";
 import { MovieListWrapper } from "@/components/MoviesList/MovieList.styled";
 import MovieItem from "@/components/MovieItem";
-import { useEffect, useState } from "react";
+import { Movie } from "@/types/Movie";
+import { useState } from "react";
+import { sdk } from "@/graphql/client";
 
 interface MovieListProps {
-    movies: IMovie[];
-    onlyFavorites?: boolean;
+    listId: number;
+    initialMovies: Movie[];
 }
 
-export default function MovieList({ movies, onlyFavorites }: MovieListProps) {
-    const [favoriteIds, setFavoriteIds] = useState<number[]>([]);
+export default function MovieList({ listId, initialMovies }: MovieListProps) {
+    const [movies, setMovies] = useState<Movie[]>(initialMovies);
 
-    useEffect(() => {
-        const fetchFavoriteMovies = async () => {
-            const response = await fetch("/api/favorites", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-            });
-            const data = await response.json();
-            setFavoriteIds(data.favoriteMovies);
-        };
-        fetchFavoriteMovies();
-    }, []);
-
-    const handleToggleFavorite = async (movieId: number) => {
-        const response = await fetch("/api/favorites", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ id: movieId }),
-        });
-        if (response.ok) {
-            const data = await response.json();
-            setFavoriteIds(data.favoriteMovies);
-        }
-    };
+    async function handleRemoveMovies(movieId: number) {
+        if (await sdk.removeMovie({ listId: listId, movieId: movieId }))
+            setMovies(movies.filter((movie) => movie.id !== movieId));
+    }
 
     return (
         <MovieListWrapper>
             {movies.map((movie, index) => (
                 <MovieItem
-                    key={`${movie.id}-${index}`}
+                    key={`${movie.movie.imdbID}-${index}`}
                     movie={movie}
                     index={index}
-                    isFavorite={favoriteIds.includes(movie.id)}
-                    onToggleFavorite={() => handleToggleFavorite(movie.id)}
-                    onlyFavorites={onlyFavorites}
+                    onRemove={() => handleRemoveMovies(movie.id)}
                 />
             ))}
         </MovieListWrapper>
