2019517ba6fbaac5062ebd6071aeef27253b1000
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-24 19:32:43 +0100

    fix/list

--- src/components/List/List.styled.ts
+++ src/components/List/List.styled.ts
@@ -1,6 +1,7 @@
 "use client";
 
 import styled from "styled-components";
+import Link from "next/link";
 
 export const ListWrapper = styled.div`
     display: flex;
@@ -12,4 +13,8 @@ export const ListWrapper = styled.div`
     margin-right: 2vw;
     width: 76vw;
     padding: 2vw 3vw;
+`;
+
+export const StyledLink = styled(Link)`
+    text-decoration: none;
 `;
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,4 @@
-import { ListWrapper } from "@/components/List/List.styled";
+import { ListWrapper, StyledLink } from "@/components/List/List.styled";
 import ListItem from "@/components/List/ListItem";
 import AddListItem from "@/components/List/AddListItem";
 import IMovieList from "@/interfaces/IMovieList";
@@ -7,7 +7,9 @@ export default function List({ movieLists }: { movieLists: IMovieList[] }) {
     return (
         <ListWrapper>
             {movieLists.map((movieList) => (
-                <ListItem key={movieList.id} movieList={movieList} />
+                <StyledLink href={`/lists/${movieList.id}`} key={movieList.id}>
+                    <ListItem key={movieList.id} movieList={movieList} />
+                </StyledLink>
             ))}
             <AddListItem />
         </ListWrapper>
