76675669111ab73b0b0075c46911996c089a6318
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-26 01:56:31 +0100

    delete/useless-files

--- src/hooks/useGetMovies.ts
+++
@@ -1,9 +0,0 @@
-import MovieListData from "@/data/MovieList.json";
-import IMovie from "@/interfaces/IMovie";
-
-export default function useGetMovies({ ids }: { ids: number[] }): IMovie[] {
-    return ids.map((id) => ({
-        ...(MovieListData as Omit<IMovie[], "id">)[id - 1],
-        id,
-    }));
-}
