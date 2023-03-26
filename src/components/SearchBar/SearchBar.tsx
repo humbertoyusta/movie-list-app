fc1457c8e7bb7736b05af71143991b73ff42a717
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-26 01:53:46 +0100

    feat/modify-search-bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,42 +1,18 @@
 import {
-    SearchBarFormStyled,
-    SearchBarInputStyled,
-    SearchBarSubmitStyled,
+    SearchBarIcon,
+    SearchBarInput,
+    SearchBarWrapper,
 } from "./SearchBar.styled";
-import { useForm } from "react-hook-form";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-export default function SearchBar() {
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
-    const router = useRouter();
-    const { register, handleSubmit } = useForm<{ search: string }>();
-    const active = searchParams.get("search") !== null;
-
-    function handleSearchSubmit({ search }: { search?: string }) {
-        const newSearchParams = new URLSearchParams(searchParams);
-        newSearchParams.delete("search");
-        if (search && search !== "") newSearchParams.set("search", search);
-        router.push(`${pathname}?${newSearchParams.toString()}`);
-    }
-
+export default function SearchBar({
+    setSearchValue,
+}: {
+    setSearchValue: (value: string) => void;
+}) {
     return (
-        <SearchBarFormStyled onSubmit={handleSubmit(handleSearchSubmit)}>
-            <SearchBarInputStyled
-                type="text"
-                placeholder="Search for amazing movies..."
-                {...register("search", { required: false })}
-            />
-            <SearchBarSubmitStyled
-                type={active ? "button" : "submit"}
-                // submit the form without the search parameter if active is true
-                onClick={
-                    active
-                        ? () => handleSearchSubmit({})
-                        : handleSubmit(handleSearchSubmit)
-                }
-                active={active}
-            />
-        </SearchBarFormStyled>
+        <SearchBarWrapper>
+            <SearchBarIcon />
+            <SearchBarInput onChange={(e) => setSearchValue(e.target.value)} />
+        </SearchBarWrapper>
     );
 }
