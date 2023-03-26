064dd8b32d83466d63b0cad3913d3dd0c001034e
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-26 20:02:58 +0200

    feat/add-gray-icon-buttons

--- src/components/Buttons/IconButton/IconButton.styled.ts
+++ src/components/Buttons/IconButton/IconButton.styled.ts
@@ -1,14 +1,21 @@
 "use client";
 
 import styled from "styled-components";
+import { theme } from "@/theme";
 
-export const IconButtonWrapper = styled.button`
-    background-color: #fff;
-    border: 1px solid #fff;
-    padding: 6px 12px;
+export const IconButtonWrapper = styled.button<{
+    color?: "gray";
+    noPadding?: boolean;
+}>`
+    background-color: ${({ color }) =>
+        color && color === "gray" ? theme.colors.gray : "#fff"};
+    border: 1px solid
+        ${({ color }) =>
+            color && color === "gray" ? theme.colors.gray : "#fff"};
+    padding: ${({ noPadding }) => (noPadding ? "0" : "6px 12px")};
 
     :hover {
-        border: 1px solid #ccc;
+        border: 1px solid ${theme.colors.gray};
         border-radius: 30%;
     }
 `;
--- src/components/Buttons/IconButton/IconButton.tsx
+++ src/components/Buttons/IconButton/IconButton.tsx
@@ -1,17 +1,30 @@
 import { IconButtonWrapper } from "@/components/Buttons/IconButton/IconButton.styled";
 import { TiArrowBack } from "react-icons/ti";
 import { AiOutlineClose } from "react-icons/ai";
+import { MdPlaylistAdd } from "react-icons/all";
 
 interface IButtonProps {
-    kind: "back" | "close";
+    kind: "back" | "close" | "add";
     onClick?: () => void;
+    color?: "gray";
+    noPadding?: boolean;
 }
 
-export default function IconButton({ kind, onClick }: IButtonProps) {
+export default function IconButton({
+    kind,
+    onClick,
+    color,
+    noPadding,
+}: IButtonProps) {
     return (
-        <IconButtonWrapper onClick={onClick}>
+        <IconButtonWrapper
+            color={color}
+            onClick={onClick}
+            noPadding={noPadding}
+        >
             {kind === "back" && <TiArrowBack size={28} />}
-            {kind === "close" && <AiOutlineClose size={28} />}
+            {kind === "close" && <AiOutlineClose size={noPadding ? 20 : 28} />}
+            {kind === "add" && <MdPlaylistAdd size={24} />}
         </IconButtonWrapper>
     );
 }
